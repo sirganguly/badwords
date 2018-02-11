1cfd6ad544314f92ba604fe15f766e6613c3ef45
Darshit Patel <darshit12@gmail.com>
2018-02-11 11:22:03 +0000

    added routes for hindi and english words

--- app.py
+++ app.py
@@ -14,6 +14,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory, session, jsonify, session, send_from_directory
 from werkzeug import secure_filename
 import os
+import random
+from randomGenerator import getHindiWord, getEnglishWord
 from datetime import datetime
 
 ###################
@@ -41,5 +43,23 @@ db = SQLAlchemy(app)
 def index():
 	return render_template('index.html')
 
+@app.route('/randHindi', methods=['GET'])
+def randHindi():
+	if request.method == 'GET':
+		hindi = getHindiWord()
+		return render_template('index.html', hindi = hindi)
+	else:
+		error = "Invalid"
+		return render_template('index.html', error = error)
+		
+@app.route('/randEnglish', methods=['GET'])
+def randEnglish():
+	if request.method == 'GET':
+		english = getEnglishWord()
+		return render_template('index.html', english = english)
+	else:
+		error = "Invalid"
+		return render_template('index.html', error = error)
+		
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8080, debug=True)
